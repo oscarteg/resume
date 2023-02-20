3084f3649cbdd2db6294ba364ab550905350c57c
Oscar te Giffel <oscar@tegiffel.com>
2023-02-20 14:41:10 +0100

    feat: updated resume

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import handlebars from "vite-plugin-handlebars";
+import { resolve } from "path";
+
+export default {
+  plugins: [handlebars({ partialDirectory: resolve(__dirname, "partials") })],
+};
