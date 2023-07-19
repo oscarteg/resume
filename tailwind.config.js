d54dd1cafd1572c99d492608a4b82889d3774e16
Oscar te Giffel <oscar@tegiffel.com>
2023-07-19 15:58:36 +0200

    feat: added compact page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./index.html"],
+  content: ["./src/**/*.html", "./pages/**/*.html"],
   theme: {
     extend: {
       fontFamily: {
--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,17 @@ import handlebars from "vite-plugin-handlebars";
 import { resolve } from "path";
 
 export default {
-  plugins: [handlebars({ partialDirectory: resolve(__dirname, "partials") })],
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "src/partials"),
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        compact: resolve(__dirname, "compact.html"),
+      },
+    },
+  },
 };
