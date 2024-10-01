eac3b777b0448e158d698e2b136c510ac2959388
Oscar te Giffel <oscar@tegiffel.com>
2024-10-01 03:04:56 +0200

    feat: update resume

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  content: ["./src/**/*.html", "./pages/**/*.html"],
-  theme: {
-    extend: {
-      fontFamily: {
-        header: ["Proza Libre"],
-        body: ["Merriweather"],
-      },
-    },
-  },
-  plugins: [],
+	content: ["./src/**/*.html", "./pages/**/*.html"],
+	theme: {
+		extend: {
+			fontFamily: {
+				header: ["Proza Libre"],
+				body: ["Merriweather"],
+			},
+		},
+	},
+	plugins: [],
 };
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import handlebars from "vite-plugin-handlebars";
-import { resolve } from "path";
+import { resolve } from "node:path";
 
 export default {
-  plugins: [
-    handlebars({
-      partialDirectory: resolve(__dirname, "src/partials"),
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        compact: resolve(__dirname, "compact.html"),
-      },
-    },
-  },
+	plugins: [
+		handlebars({
+			partialDirectory: resolve(__dirname, "src/partials"),
+		}),
+	],
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				compact: resolve(__dirname, "compact.html"),
+			},
+		},
+	},
 };
